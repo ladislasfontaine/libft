c93a59845f9267e97a5464968e03b967e5388b88
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-05-04 08:37:25 +0200

    minor correction

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lafontai <lafontai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 17:07:56 by lafontai          #+#    #+#             */
-/*   Updated: 2020/05/04 08:27:24 by lafontai         ###   ########.fr       */
+/*   Updated: 2020/05/04 08:36:17 by lafontai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	after = 0;
 	while (s1[before] && ft_strchr(set, (int)s1[before]))
 		before++;
-	if (t_len - before >= 0)
+	if (t_len - before <= 0)
 		return (ft_substr(s1, 0, 0));
 	while (t_len - 1 - after >= 0 && ft_strchr(set, (int)s1[t_len - 1 - after]))
 		after++;
